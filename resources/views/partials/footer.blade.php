18d3a881f81ad7931a01d0a82446e67208182252
MyUsername <figusgiacomo04@gmail.com>
2024-03-13 16:03:06 +0100

    FEAT!: header-footer (static-components)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,71 @@
+<footer>
+    <div id="top-footer">
+        <div id="container-top-footer" class="container mx-auto">
+            <div class="flex">
+                <div class="pe-6">
+                    <h4 class="text-bold text-xl">DC COMICS</h4>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>News</li>
+                        <li>Videos</li>
+                    </ul>
+
+                    <h4 class="text-bold text-xl mt-6">SHOP</h4>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collecitbles</li>
+                    </ul>
+                </div>
+
+                <div class="pe-6">
+                    <h4 class="text-bold text-xl">DC</h4>
+                    <ul>
+                        <li>Terms of use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4 class="text-bold text-xl">SITES</h4>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Universe</li>
+                        <li>DC Kids</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div id="bottom-footer">
+        <div class="container mx-auto flex justify-between">
+            <button>
+                SIGN UP NOW
+            </button>
+            
+            <div>
+                <span>FOLLOW US</span>
+                <i class=""></i>
+                
+
+            </div>
+        </div>
+    </div>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header >
+    <nav class="container mx-auto">
+        <figure>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+        </figure>    
+
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
+    </nav>
+
+</header>
